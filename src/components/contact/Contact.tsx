d434262511402e0032394d7f0f7ee3a4c4f3b4e0
Ankita Varigonda <avarigonda18@gmail.com>
2026-03-06 22:06:15 -0500

    refac

--- src/components/contact/Contact.tsx
+++ src/components/contact/Contact.tsx
@@ -1,49 +1,30 @@
-import { Box, Button, Center, Flex, useClipboard } from "@chakra-ui/react";
-import ColorMode from "../generic/ColorMode";
-import { MouseFollower } from "../generic/Mouse";
-import SideBar from "../generic/SideBar";
+import { Button, Center, useClipboard } from "@chakra-ui/react";
 import { LuCheck, LuCopy } from "react-icons/lu";
-import Footer from "../generic/Footer";
 
-interface Props {
-  pages: string[];
-  currentPage: string;
-  onClick: (page: string) => void;
-}
-
-function Contact({ pages, currentPage, onClick }: Props) {
+function Contact() {
   const { copied, copy } = useClipboard({ value: "avarigon@andrew.cmu.edu" });
 
   return (
     <>
-      <Flex direction="column" minH="100vh">
-        <Box flex="1">
-          <Center mt={10} fontSize="6xl">
-            Contact
-          </Center>
-          <Center m={5} mt={10} fontSize="xl">
-            You can email me at:
-          </Center>
-          <Center>
-            <Button
-              variant="outline"
-              onClick={copy}
-              minW="250px"
-              bg="whiteAlpha.50"
-              borderColor="whiteAlpha.300"
-              _hover={{ bg: "whiteAlpha.200", borderColor: "purple.400" }}
-            >
-              {copied ? <LuCheck color="green" /> : <LuCopy />}
-              {copied ? "Copied to Clipboard!" : "avarigon@andrew.cmu.edu"}
-            </Button>
-          </Center>
-        </Box>
-        <Footer />
-      </Flex>
-
-      <ColorMode />
-      <SideBar currentPage={currentPage} onClick={onClick} pages={pages} />
-      <MouseFollower />
+      <Center mt={10} fontSize="6xl">
+        Contact
+      </Center>
+      <Center m={5} mt={10} fontSize="xl">
+        You can email me at:
+      </Center>
+      <Center>
+        <Button
+          variant="outline"
+          onClick={copy}
+          minW="250px"
+          bg="whiteAlpha.50"
+          borderColor="whiteAlpha.300"
+          _hover={{ bg: "whiteAlpha.200", borderColor: "purple.400" }}
+        >
+          {copied ? <LuCheck color="green" /> : <LuCopy />}
+          {copied ? "Copied to Clipboard!" : "avarigon@andrew.cmu.edu"}
+        </Button>
+      </Center>
     </>
   );
 }
--- src/components/puzzles/Puzzles.tsx
+++ src/components/puzzles/Puzzles.tsx
@@ -1,46 +1,21 @@
-import { Box, Center, Flex, SimpleGrid } from "@chakra-ui/react";
-import ColorMode from "../generic/ColorMode";
-import { MouseFollower } from "../generic/Mouse";
-import SideBar from "../generic/SideBar";
-import Footer from "../generic/Footer";
-import { ScrollToTop } from "../generic/ScrollTop";
+import { Center, SimpleGrid } from "@chakra-ui/react";
 import { gallery } from "./Gallery";
 import Gcard from "./Gcard";
 
-interface Props {
-  pages: string[];
-  currentPage: string;
-  onClick: (page: string) => void;
-}
-
-function Puzzles({ pages, currentPage, onClick }: Props) {
+function Puzzles() {
   return (
     <>
-      <Flex direction="column" minH="100vh">
-        <Box flex="1">
-          <Center mt={10} fontSize="6xl">
-            Puzzles
-          </Center>
-          <Center mt={10} fontSize="xl">
-            Here are some of the puzzles I've done!
-          </Center>
-          <SimpleGrid
-            columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
-            gap={6}
-            padding={10}
-          >
-            {gallery.map((img) => (
-              <Gcard key={img.id} g={img} />
-            ))}
-          </SimpleGrid>
-        </Box>
-        <Footer />
-      </Flex>
-
-      <ScrollToTop />
-      <ColorMode />
-      <SideBar currentPage={currentPage} onClick={onClick} pages={pages} />
-      <MouseFollower />
+      <Center mt={10} fontSize="6xl">
+        Puzzles
+      </Center>
+      <Center mt={10} fontSize="xl">
+        Here are some of the puzzles I've done!
+      </Center>
+      <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 4 }} gap={6} padding={10}>
+        {gallery.map((img) => (
+          <Gcard key={img.id} g={img} />
+        ))}
+      </SimpleGrid>
     </>
   );
 }
